035cacc394cf54c9b97fbd8941b1aaf7d209f9ef
Gospel Kizamba <gospel14.es@gmail.com>
2026-03-24 11:48:34 +0100

    ajout de la page de connexion login

---
+++ ProjetSicilyLines/Login.xaml.cs
@@ -0,0 +1,14 @@
+namespace ProjetSicilyLines;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+       
+    }
+}
