443f281aa1ca3005db5906cf55916626864f4eef
Gospel Kizamba <gospel14.es@gmail.com>
2026-03-24 11:25:30 +0100

    ajout du controller loginController

---
+++ APISicilyLines/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace APISicilyLines.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class LoginController : ControllerBase
+    {
+
+
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUser/{pseudo}/{pass}")]
+
+
+        public bool Get(string pseudo, string pass)
+
+        {
+
+            if (pseudo == "Gospel" && pass == "Gospel14")
+
+            {
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+
+
+    }
+}
