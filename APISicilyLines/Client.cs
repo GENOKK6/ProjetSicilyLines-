23408c57ddee76a4cd9aa4f5f630149ee18d4f9e
Gospel Kizamba <gospel14.es@gmail.com>
2026-03-24 15:48:40 +0100

    j'ai ajouter le fichier client

---
+++ APISicilyLines/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace APISicilyLines
+{
+    public class Client
+    {
+  public int Id { get; set; }
+        public string Nom { get; set; }        
+        public string Prenom { get; set; }     
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Adresse { get; set; }
+        public string Ville { get; set; }
+        public string CodePostal { get; set; }
+        public string MotDePasse { get; set; }
+    }
+}
